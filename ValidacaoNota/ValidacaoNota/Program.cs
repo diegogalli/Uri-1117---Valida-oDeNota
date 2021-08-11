2515493fc3354a0e50d222e08033fd146694bb95
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:50:41 -0300

    Uri 1117 Validacao de Nota

---
+++ ValidacaoNota/ValidacaoNota/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValidacaoNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (n1 < 0.0 || n1 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (n2 <0.0 || n2 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double media = (n1 + n2) / 2.0;
+            Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+        }   
+    }
+}
